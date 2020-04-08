7ab6c2767c39e239f0c0dd695946b3996b62672c
cainy-a <cainatkinson98@gmail.com>
2020-04-08 14:31:04 +0100

    Refactor Farenheit<->Celsius Converters to be one line of code.

--- CainsSharpTools/Convert.cs
+++ CainsSharpTools/Convert.cs
@@ -9,20 +9,14 @@
             /// </summary>
             /// <param name="fahrenheit"></param>
             /// <returns>celsius</returns>
-            public static float FahrenheitToCelsius(float fahrenheit)
-            {
-                return (fahrenheit - 32) / 1.8f;
-            }
+            public static float FahrenheitToCelsius(float fahrenheit) => (fahrenheit - 32) / 1.8f;
 
             /// <summary>
             /// Converts from celsius to fahrenheit
             /// </summary>
             /// <param name="celsius"></param>
             /// <returns>fahrenheit</returns>
-            public static float CelsiusToFahrenheit(float celsius)
-            {
-                return (celsius * 1.8f) + 32;
-            }
+            public static float CelsiusToFahrenheit(float celsius) => (celsius * 1.8f) + 32;
         }
     }
 }
